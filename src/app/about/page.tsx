3096de9fef92d094b1fa1713bfd8866f019750d2
Billy Alexander <billyalexndr@gmail.com>
2024-11-18 17:57:35 +0700

    materi pertemuan 8

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    throw new Error();
+    return (
+        <div>
+            <p>This is the about page.</p>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'My Next JS APP',
+    description: 'Belajar Next JS',
+    keywords: 'learn, next js, study club',
+};
 
 export default function HomePage() {
+    // throw new Error();
     return (
         <div>
-            <h1>Welcome to Our Website</h1>
-            <p>Explore our products and services.</p>
+            <h1>Welcome to My Next.js App</h1>
+            <p>This is the home page.</p>
         </div>
     );
 }
